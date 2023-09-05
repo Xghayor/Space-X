5063c83e90af961a1ea1bb2b9fdec65c76b96600
redwan <redtah2007@yahoo.com>
2023-09-05 07:53:20 -0700

    Implement Rocket API and CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   BrowserRouter, Routes, Route, Outlet,
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Rockets from './components/Rockets';
+import { fetchRockets } from './Redux/rockets/rocketsSlice';
 
 function Layout() {
   return (
@@ -13,11 +17,16 @@ function Layout() {
 }
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<div>Rockets</div>} />
+          <Route exact path="/" element={<Rockets />} />
           <Route path="missions" element={<div>Missions</div>} />
           <Route path="mypage" element={<div>My Page</div>} />
           <Route path="*" element={<div>If page not found it goes here</div>} />
